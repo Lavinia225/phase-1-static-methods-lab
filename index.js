b250952fb2a1985daa78e3426025b0ce0661de6a
luna seifert <thepuddingdeity@gmail.com>
2022-10-18 14:56:32 -0500

    commit tree dd8f07f4fd9be73c331c603a750cafbce6219c5a

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt().toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "")
+  }
+
+  static titleize(string){
+    const arrayOfWords = string.split(' ')
+    const excludedWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    arrayOfWords[0] = this.capitalize(arrayOfWords[0])
+    const capitalizedWords = arrayOfWords.map(word =>{
+      if (excludedWords.indexOf(word) === -1){
+        return this.capitalize(word)
+      }
+      else{
+        return word
+      }
+    })
+    return capitalizedWords.join(" ")
+  }
 }
+
+// the, a, an, but, of, and, for, at, by, and from; first word always capped
+const nyan = "the words within the heart are the most important of all"
+
+console.log(Formatter.titleize(nyan))
